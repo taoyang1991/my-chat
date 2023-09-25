35ab5c0dbe4ab75a0ba7386b44a6aec25b5d7006
zhaixing <zhaixing@pinduoduo.com>
2023-09-25 20:54:17 +0800

    add

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModel
 from transformers.utils import logging
-from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
 from langchain import LLMChain, PromptTemplate
 from langchain.llms.base import LLM
 from pydantic import Field
@@ -54,12 +54,15 @@ if __name__ == "__main__":
     memory.chat_memory.add_user_message("你好!")
     memory.chat_memory.add_ai_message("你好， 我是你的私人AI助手小I")
 
+    memory.chat_memory.add_user_message("我叫张骞，你叫什么？")
+    memory.chat_memory.add_ai_message("我叫小I")
+
     llm_chain = LLMChain(
         llm=chatglm2,
         prompt=prompt,
         verbose=True,
         memory=memory,
     )
-    human_input = "你好"
+    human_input = "我叫什么？你叫什么？"
     response = llm_chain.predict(human_input=human_input)
     print(response)
